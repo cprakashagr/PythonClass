9c26efd94ba16093b97ce5600d3c2075b91d4fe3
Chandra Prakash <cprakashagr@gmail.com>
2016-09-02 16:02:16 +0530

    Basic CommandLine Example

---
+++ src/random/CommandLineArguments.py
@@ -0,0 +1,5 @@
+import sys
+
+if __name__ == '__main__':
+    print (sys.argv[0])     #  Prints the file name if executed as : ~  python pythonFile.py
+    # print sys.argv[1];   #  Throws exception.
